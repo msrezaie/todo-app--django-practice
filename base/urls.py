972fdab61a1a115a4285bc21002ca31556e0d4e4
Mohammad Sulayman Rezaie <79583632+msrezaie@users.noreply.github.com>
2022-09-09 22:11:53 -0400

    Added tasks list and details
    Added functionalities to list the created tasks and view their details

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='test'),
+    path('', views.TaskList.as_view(), name='task-list'),
+    path('task-detail/<int:pk>', views.TaskDetail.as_view(), name='task-detail'),
+    # path('task-create/<int:pk>', views.TaskCreate.as_view(), name='task-create')
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from . models import Task
 
-# Create your views here.
-def test(request):
-    return HttpResponse("hello world")
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'base/list.html'
+
+class TaskDetail(DetailView):
+    model = Task
+    template_name: str = 'base/detail.html'
