bae8ee7580f54100f1afbf91ec387a685c45b296
Mohammad Sulayman Rezaie <79583632+msrezaie@users.noreply.github.com>
2022-09-12 17:31:27 -0400

    Updated 'base' urls.py

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tasks/', views.TaskList.as_view(), name='task-list'),
+    path('', views.TaskList.as_view(), name='task-list'),
     path('task-detail/<int:pk>', views.TaskDetail.as_view(), name='task-detail'),
     path('task-create/', views.TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>', views.TaskUpdate.as_view(), name='task-update'),
