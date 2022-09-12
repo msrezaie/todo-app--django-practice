b86dc2855c7908cfacd9daee211451779b7c7126
Mohammad Sulayman Rezaie <79583632+msrezaie@users.noreply.github.com>
2022-09-12 18:22:30 -0400

    Updated urls.py
    Included 'task-list/' for the tasks list page url representing the home page after a user has logged in

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    path('', views.TaskList.as_view(), name='task-list'),
+    path('task-list/', views.TaskList.as_view(), name='task-list'),
     path('task-detail/<int:pk>', views.TaskDetail.as_view(), name='task-detail'),
     path('task-create/', views.TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>', views.TaskUpdate.as_view(), name='task-update'),
-    path('task-delete/<int:pk>', views.TaskDelete.as_view(), name='task-delete')
+    path('task-delete/<int:pk>', views.TaskDelete.as_view(), name='task-delete'),
+    path('', RedirectView.as_view(url='task-list/', permanent=True))
 ]
