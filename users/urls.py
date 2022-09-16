e489ff16b4a9fe5e0725b43f6fad34c94bfe2164
Mohammad Sulayman Rezaie <79583632+msrezaie@users.noreply.github.com>
2022-09-16 00:05:53 -0400

    Added user registration functionality
    Added a new html page that includes a form dedicated for new users registration with its functionalities

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('task-login/', views.TaskLoginView.as_view(), name='task-login'),
-    path('task-logout/', LogoutView.as_view(next_page='task-login'), name='task-logout')
+    path('task-logout/', LogoutView.as_view(next_page='task-login'), name='task-logout'),
+    path('task-register/', views.RegisterView.as_view(), name='task-register')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,10 @@
+from django.shortcuts import redirect
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 class TaskLoginView(LoginView):
     template_name = 'users/login.html'
@@ -8,3 +13,19 @@ class TaskLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('task-list')
+    
+class RegisterView(FormView):
+    template_name = 'users/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('task-list')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user:
+            login(self.request, user)
+        return super().form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('task-list')
+        return super().get(*args, **kwargs)
