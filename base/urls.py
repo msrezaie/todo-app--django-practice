bc8545201f31334f646c52c5958ff992e91c22e1
Mohammad Sulayman Rezaie <79583632+msrezaie@users.noreply.github.com>
2022-09-16 21:15:31 -0400

    Fixed a url redirect issue
    Accessing the url didn't redirect to the login page, the usage of a RedirectView in the urlpatterns was needed

--- base/urls.py
+++ base/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('task-detail/<int:pk>', views.TaskDetail.as_view(), name='task-detail'),
     path('task-create/', views.TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>', views.TaskUpdate.as_view(), name='task-update'),
-    path('task-delete/<int:pk>', views.TaskDelete.as_view(), name='task-delete')
+    path('task-delete/<int:pk>', views.TaskDelete.as_view(), name='task-delete'),
+
+    path('', RedirectView.as_view(url='task-list/'))
 ]
